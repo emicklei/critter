0915cd21bb3659538028b1131f7e77759942a733
jcraane <jcraane@gmail.com>
2013-12-06 16:20:23 +0100

    Added RuleDaoFileImpl.java, empty implementation.

---
+++ src/main/java/com/philemonworks/critter/dao/file/RuleDaoFileImpl.java
@@ -0,0 +1,33 @@
+package com.philemonworks.critter.dao.file;
+
+import com.philemonworks.critter.dao.RuleDao;
+import com.philemonworks.critter.rule.Rule;
+
+import java.util.List;
+
+/**
+ * Insert documentation here.
+ *
+ * @author jcraane
+ */
+public class RuleDaoFileImpl implements RuleDao {
+    @Override
+    public List<Rule> getRules() {
+        return null;
+    }
+
+    @Override
+    public void addOrReplaceRule(final Rule aRule) {
+
+    }
+
+    @Override
+    public Rule getRule(final String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteRule(final String id) {
+
+    }
+}
