2c6150da4095094fa9beaafb83db5b89b312d125
Ernest Micklei <ernest.micklei@gmail.com>
2014-04-16 15:03:36 +0200

    update footer in UI

--- src/main/java/com/philemonworks/critter/ui/PageFooter.java
+++ src/main/java/com/philemonworks/critter/ui/PageFooter.java
@@ -27,7 +27,7 @@ public class PageFooter implements Renderable {
             html._div();
             
             html.div(class_("ui-block-c"));
-                html.h5(class_("ui-btn-right").style("font-size:small")).content("(c) 2013, philemonworks.com - " + Utils.getMavenVersion());
+                html.h5(class_("ui-btn-right").style("font-size:small")).content("github.com/emicklei/critter - " + Utils.getMavenVersion());
             html._div()            
             ._div();
     }
