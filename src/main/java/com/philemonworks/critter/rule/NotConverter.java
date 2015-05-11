a28d99a777bc0bd2655c0744c96ebce9b0e35e5b
Jeroen Ruijgers <jruijgers@bol.com>
2015-05-11 17:26:28 +0200

    update the NotConverter to match the schema
    added some missing fields when unmarshalling and updated the
    marschalling part to give correct xml
    
    Change-Id: I082336084c9adcc43ed790650fec400fb4298105

--- src/main/java/com/philemonworks/critter/rule/NotConverter.java
+++ src/main/java/com/philemonworks/critter/rule/NotConverter.java
@@ -1,13 +1,6 @@
 package com.philemonworks.critter.rule;
 
-import com.philemonworks.critter.condition.BasicAuthentication;
-import com.philemonworks.critter.condition.Condition;
-import com.philemonworks.critter.condition.Equals;
-import com.philemonworks.critter.condition.RequestHeader;
-import com.philemonworks.critter.condition.Host;
-import com.philemonworks.critter.condition.Not;
-import com.philemonworks.critter.condition.Path;
-import com.philemonworks.critter.condition.Port;
+import com.philemonworks.critter.condition.*;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
@@ -23,6 +16,12 @@ public class NotConverter implements Converter {
 
     @Override
     public void marshal(Object aNot, HierarchicalStreamWriter writer, MarshallingContext context) {
+        Not not = (Not) aNot;
+        if (not.condition != null) {
+            writer.startNode(not.condition.getClass().getSimpleName().toLowerCase());
+            context.convertAnother(not.condition);
+            writer.endNode();
+        }
     }
 
     @Override
@@ -30,19 +29,20 @@ public class NotConverter implements Converter {
         Not not = new Not();
         reader.moveDown();
         this.convertNext(reader, ctx, not, "host", Host.class);
-        this.convertNext(reader, ctx, not, "equals", Equals.class);
-        this.convertNext(reader, ctx, not, "Header", RequestHeader.class);
-        this.convertNext(reader, ctx, not, "not", Not.class);
+        this.convertNext(reader, ctx, not, "method", Method.class);
         this.convertNext(reader, ctx, not, "path", Path.class);
         this.convertNext(reader, ctx, not, "port", Port.class);
+        this.convertNext(reader, ctx, not, "header", RequestHeader.class);
         this.convertNext(reader, ctx, not, "basicauthentication", BasicAuthentication.class);
+        this.convertNext(reader, ctx, not, "xpath", XPath.class);
         reader.moveUp();
+
         return not;
     }
 
     private void convertNext(HierarchicalStreamReader reader, UnmarshallingContext ctx, Not not, String alias, Class<?> conditionClass) {
         if (alias.equals(reader.getNodeName())) {
-            not.condition = (Condition)ctx.convertAnother(not,conditionClass);
+            not.condition = (Condition) ctx.convertAnother(not, conditionClass);
         }
     }
 
