6932226abbc5b540b15d6f800cc9ee55da654e2c
Jeroen Ruijgers <jruijgers@bol.com>
2015-05-26 11:23:34 +0200

    change matchers for records
    path adn query are optional params for the request, so it is valid that
    they are empty
    
    Change-Id: I2d658d420246338f1d33bf5cdcfb77623db93b9b

--- src/main/java/com/philemonworks/critter/dao/RecordingDaoMemoryImpl.java
+++ src/main/java/com/philemonworks/critter/dao/RecordingDaoMemoryImpl.java
@@ -18,8 +18,8 @@ public class RecordingDaoMemoryImpl implements RecordingDao {
     public List<Recording> search(String host, String method, String path, String query, int limit) {
         List<Recording> result = new ArrayList<Recording>();
         for (Recording each : this.recordings) {
-            if ((!host.isEmpty() && host.equals(each.host)) && (!method.isEmpty() && method.equals(each.method)) && (!path.isEmpty() && path.equals(each.path))
-                    && (!query.isEmpty() && query.equals(each.query))) {
+            if ((!host.isEmpty() && host.equals(each.host)) && (!method.isEmpty() && method.equals(each.method)) && (path.isEmpty() || path.equals(each.path))
+                    && (query.isEmpty() || query.equals(each.query))) {
                 result.add(each);
                 if (limit > 0 && result.size() == limit) {
                     return result; // early exit
