5c1134c547d01b0a691fe98be230da6756dc3b5b
Ernest Micklei <ernest.micklei@gmail.com>
2016-04-03 21:57:46 +0200

    updating sys chk

--- systemcheck/proto.go
+++ systemcheck/proto.go
@@ -9,16 +9,30 @@ import (
 
 const someComplex = "CgpoZWxsbyBzb21lEAAYKiXD9UhAKNKF2MwEMCowVDoCCERCAggXSgIIAFINCgtoZWxsbyBwcm90b1oICgYICBAmGCpiCQoHCOAPEAMYEmoFdG9rZW5yEwoRCgcI4A8QDBgUEgYICBApGCqCAQdtaXNzaW5nigEKCgNvbmUKA3R3b5IBCQoHCgUIoQEQApoBBUhFTExPogECCCqqAQcKBXRocmVl"
 
+var (
+	t   = TestingT
+	api = NewClient("http://localhost:8877", new(http.Client))
+)
+
 func main() {
-	t := TestingT
-	api := NewClient("http://localhost:8877", new(http.Client))
+	createRule()
+	registerProtos()
+	reportDayTime()
+}
+
+func createRule() {
+	r := api.POST(t, NewConfig("/rules/fixed-1/proto").Read(f))
+	Dump(t, r)
+}
+
+func registerProtos() {
 	f, _ := os.Open("../src/test/resources/bol-xsdtypes-1.6.proto")
 	defer f.Close()
-	cfg := NewConfig("/rules/fixed-1/proto")
-	cfg.BodyReader = f
-	r := api.POST(t, cfg)
+	r := api.POST(t, NewConfig("/rules/fixed-1/proto").Read(f))
 	Dump(t, r)
+}
 
+func reportDayTime() {
 	cfg = NewConfig("/rules/fixed-1/proto/xsdtypes.DayTime")
 	r = api.GET(t, cfg)
 	Dump(t, r)
